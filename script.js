480bc8ebe566aa182623b9b57ce7c4dc2e6a1c30
Denis Homoki <homoki.denisth@gmail.com>
2022-08-17 21:31:17 +0300

    added styles and functionality

---
+++ script.js
@@ -0,0 +1,34 @@
+$(function () {
+  let homeScore = 0;
+  let guestScore = 0;
+
+  $(".buttons .home-btn-1").click(function () {
+    homeScore += 1;
+    $(".home-score").text(homeScore);
+  });
+
+  $(".buttons .home-btn-2").click(function () {
+    homeScore += 2;
+    $(".home-score").text(homeScore);
+  });
+
+  $(".buttons .home-btn-3").click(function () {
+    homeScore += 3;
+    $(".home-score").text(homeScore);
+  });
+
+  $(".buttons .guest-btn-1").click(function () {
+    guestScore += 1;
+    $(".guest-score").text(guestScore);
+  });
+
+  $(".buttons .guest-btn-2").click(function () {
+    guestScore += 2;
+    $(".guest-score").text(guestScore);
+  });
+
+  $(".buttons .guest-btn-3").click(function () {
+    guestScore += 3;
+    $(".guest-score").text(guestScore);
+  });
+});
